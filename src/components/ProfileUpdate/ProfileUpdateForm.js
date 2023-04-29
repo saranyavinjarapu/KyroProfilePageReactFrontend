3d3d45ebb9f86c8aff4f9566a6d1cd2db5197b5c
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2023-04-29 11:19:37 +0530

    refined profile update form for cleaner and generic code

--- src/components/ProfileUpdate/ProfileUpdateForm.js
+++ src/components/ProfileUpdate/ProfileUpdateForm.js
@@ -7,94 +7,42 @@ import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Button from "@mui/material/Button";
+
+const FormItem = ({ label, icon }) => {
+  return (
+    <TextField
+      label={label}
+      size="small"
+      InputLabelProps={{
+        shrink: true,
+      }}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">{icon}</InputAdornment>
+        ),
+      }}
+    />
+  );
+};
 
 const ProfileUpdateForm = () => {
   return (
     <form className={styles.ProfileUpdateForm}>
-      <TextField
-        label="First Name"
-        size="small"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <PersonIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
-      <TextField
-        label="Last Name"
-        size="small"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <PersonIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
-      <TextField
-        label="Display Name"
-        size="small"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <AccountBoxIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
-      <TextField
-        label="Email"
-        size="small"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <MailOutlineIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
-      <TextField
-        label="Phone No(Work)"
-        size="small"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <LocalPhoneIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
-      <TextField
-        label="Location"
-        size="small"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <LocationOnIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
+      <div className={styles.ProfileUpdateFormBody}>
+        <FormItem label="First Name" icon={<PersonIcon />} />
+        <FormItem label="Last Name" icon={<PersonIcon />} />
+        <FormItem label="Display Name" icon={<AccountBoxIcon />} />
+        <FormItem label="Email" icon={<MailOutlineIcon />} />
+        <FormItem label="Phone No(Work)" icon={<LocalPhoneIcon />} />
+        <FormItem label="Phone No(Personal)" icon={<LocalPhoneIcon />} />
+        <FormItem label="Location" icon={<LocationOnIcon />} />
+      </div>
+      <div className={styles.ProfileUpdateFormFooter}>
+        <Button variant="contained" size="small" color="error">
+          Save Changes
+        </Button>
+      </div>
     </form>
   );
 };
